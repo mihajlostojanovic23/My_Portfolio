c80802eaabd2fa7d9a838bd9821e6933cdc2dfb9
Mihajlo <mihajlostojanovic091999@gmail.com>
2022-07-25 19:42:35 +0200

    Portfolio

--- src/components/Social_Links/Links.js
+++ src/components/Social_Links/Links.js
@@ -12,8 +12,11 @@ import ChatApp from './Chat.PNG'
 import CV from './Mihajlo_Stojanovic_CV.pdf'
 import Button from '@mui/material/Button';
 import GithubPic from './GitHub-Naslovna.png'
+import Movie from './Movie.PNG'
+import Memory from './MemoryGame.PNG'
+import MiniPtojects from './Mini_Projects.png'
+import ViewPage from './page-views.png'   
 
-   
 
 function Links() {
   
@@ -31,7 +34,8 @@ function Links() {
       {tool: 'MySQL'},
       {tool: 'CRUD'},
       {tool: 'JQuery'}
-]    },
+],
+page: 'https://night-and-day-official.netlify.app/'    },
 
 
 {
@@ -43,7 +47,8 @@ function Links() {
     {tool: 'NodeJS' },
     {tool: 'Express'},
     {tool: 'API'}
-]    },
+],
+page: 'https://weather-official.netlify.app/'    },
 
 
 
@@ -56,7 +61,8 @@ function Links() {
   {tool: 'HTML' },
   {tool: 'CSS'},
   {tool: 'JavaScript'}
-] },
+],
+page: 'https://to-do-list-official.netlify.app/' },
 
 
 {
@@ -68,7 +74,8 @@ function Links() {
   {tool: 'HTML' },
   {tool: 'CSS'},
   {tool: 'JavaScript'}
-] },
+],
+page: 'https://rock-paper-scissors-official.netlify.app/' },
 
 
 {
@@ -81,7 +88,8 @@ function Links() {
   {tool: 'NodeJS'},
   {tool: 'Socket.io'},
   {tool:'MUI'}
-] },
+],
+page: '' },
 
 {
   Name: 'C.N. Random Jokes',
@@ -93,7 +101,8 @@ function Links() {
   {tool: 'Express'},
   {tool: 'NodeJS'},
   {tool: 'API'}
-] },
+],
+page: '' },
 
 {
   Name: 'Quiz App',
@@ -104,7 +113,8 @@ function Links() {
   {tool: 'React'},
   {tool: 'API'},
   {tool: 'MUI'}
-] },
+],
+page: '' },
 
 {
   Name: 'Prodavnica Kafe',
@@ -119,7 +129,8 @@ function Links() {
   {tool: 'NodeJS'},
   {tool: 'Express'},
   {tool: 'MySQL'}
-] },
+],
+page: '' },
 
 {
   Name: 'Portfolio',
@@ -128,10 +139,41 @@ function Links() {
   Link: 'https://github.com/mihajlostojanovic23/My_Portfolio',
   Tools: [
   {tool: 'React' },
-  {tool: 'Heroku'}
-] },
+  {tool: 'Netlify'}
+],
+page: 'https://portfolio-official.netlify.app/' },
 
-
+{
+  Name: 'Movie App',
+  Picture: Movie ,
+  Description: 'Another project where my main focus was to learn API handling. This application contains the home page, trending movies as well as the ability to search for a specific movie. By clicking on a specific movie, details are read',
+  Link: 'https://github.com/mihajlostojanovic23/Movie_App',
+  Tools: [
+  {tool: 'React' },
+  {tool: 'Netlify'}
+],
+page:'https://movies-official.netlify.app/' },
+{
+  Name: 'Memory Game',
+  Picture: Memory ,
+  Description: 'This is a project where I wanted to make some kind of game like a memory game. I made a game using React and my main goal was to learn how to create animations as well as assign a class if a condition is met',
+  Link: 'https://github.com/mihajlostojanovic23/Memory_App',
+  Tools: [
+  {tool: 'React' },
+  {tool: 'Netlify'}
+],
+page:'' },
+{
+  Name: 'Mini Projects',
+  Picture: MiniPtojects ,
+  Description: 'Here are some projects I created to refresh and solidify my knowledge',
+  Link: 'https://github.com/mihajlostojanovic23/Mini_Projects',
+  Tools: [
+  {tool: 'HTML' },
+  {tool: 'CSS'},
+  {tool: 'JavaScript'}
+],
+page: '' },
 
     
     ]
@@ -146,7 +188,7 @@ function Links() {
             <div className='link'><a href='https://github.com/mihajlostojanovic23' target='_blank'>GitHub</a></div>
 
         </div>
-        <h2 className='projects_title'>My Projects</h2>
+        <h2 className='projects_title'>Some projects</h2>
        
         <div className='projects_content'>
           <div className='projects'>
@@ -155,7 +197,8 @@ function Links() {
     return <div className='project'>
              <div className='project_photo'> <a target='_blank' href={info.Link}> <img className='pic' src={info.Picture} />
              <div className='github'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
- </div>
+          {info.page != ''?  <div className='app_page'><a target='blank' href={info.page}><img className='photo_view_page'  src={ViewPage} /></a></div>  : <div> </div>}
+</div>
                </a>
                
               </div>
